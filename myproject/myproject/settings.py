3644fcbfac93b758c32160714989d81f035c157e
ven8462 <lavenderanyango9@gmail.com>
2024-09-17 18:00:23 +0300

    feat: update settings for db

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
+        'PORT': os.getenv('DB_PORT', '5432'),  
     }
 }
 
